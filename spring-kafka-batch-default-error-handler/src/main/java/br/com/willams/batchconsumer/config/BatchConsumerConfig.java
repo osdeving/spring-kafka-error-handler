f986958369fda94dc308f810d4cfd4434f28867d
Willams <osdeving@gmail.com>
2022-07-08 23:40:29 -0300

    adicionou config

---
+++ spring-kafka-batch-default-error-handler/src/main/java/br/com/willams/batchconsumer/config/BatchConsumerConfig.java
@@ -0,0 +1,33 @@
+package br.com.willams.batchconsumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+@Slf4j
+public class BatchConsumerConfig {
+
+    @Autowired
+    KafkaProperties kafkaProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(name = "batchKafkaListenerContainerFactory")
+    ConcurrentKafkaListenerContainerFactory<?, ?> batchKafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+        factory.setBatchListener(true);
+        return factory;
+    }
+}
