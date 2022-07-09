87515a76364f502634ecd3c0721a56e04f83c294
Willams <osdeving@gmail.com>
2022-07-08 22:54:14 -0300

    Initial commit

---
+++ spring-kafka-batch-default-error-handler/src/main/java/br/com/willams/batchconsumer/BatchConsumerApplication.java
@@ -0,0 +1,2 @@
+package br.com.willams.batchconsumer;public class BatchConsumerApplication {
+}
---
+++ spring-kafka-batch-default-error-handler/src/test/java/br/com/willams/batchconsumer/BatchConsumerApplicationTest.java
@@ -0,0 +1,6 @@
+package br.com.willams.batchconsumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BatchConsumerApplicationTest {
+  
+}
