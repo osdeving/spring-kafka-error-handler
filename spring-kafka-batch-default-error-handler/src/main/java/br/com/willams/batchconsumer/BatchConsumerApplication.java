0687cae754f04a4b4a1d61dd54dfe3163fd3c8a6
Willams <osdeving@gmail.com>
2022-07-08 23:36:38 -0300

    ajustes

--- spring-kafka-batch-default-error-handler/src/main/java/br/com/willams/batchconsumer/BatchConsumerApplication.java
+++ spring-kafka-batch-default-error-handler/src/main/java/br/com/willams/batchconsumer/BatchConsumerApplication.java
@@ -1,2 +1,11 @@
-package br.com.willams.batchconsumer;public class BatchConsumerApplication {
+package br.com.willams.batchconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchConsumerApplication.class, args);
+    }
 }
--- spring-kafka-batch-default-error-handler/src/test/java/br/com/willams/batchconsumer/BatchConsumerApplicationTest.java
+++ spring-kafka-batch-default-error-handler/src/test/java/br/com/willams/batchconsumer/BatchConsumerApplicationTest.java
@@ -1,6 +1,11 @@
 package br.com.willams.batchconsumer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import static org.junit.jupiter.api.Assertions.*;
 class BatchConsumerApplicationTest {
-  
+    void contenxtLoadTest() {
+
+    }
 }
